0f90344cb889627a046376d8bd86dfa0dd3b55db
kobi <kobi.b@inner-active.com>
2017-09-24 02:56:52 +0300

    separation

--- app/src/main/java/nice/com/a7boom/MainActivity.kt
+++ app/src/main/java/nice/com/a7boom/MainActivity.kt
@@ -2,12 +2,8 @@ package nice.com.a7boom
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import vm.counter.Counter
-import vm.counter.FlatList
-import vm.counter.FlatListProps
 import vm.vdom.Platform
 import vm.vdom.android.AndroidRenderer
-import vm.vnode.h
 
 class MainActivity : AppCompatActivity() {
     lateinit var androidRenderer: AndroidRenderer
@@ -17,15 +13,6 @@ class MainActivity : AppCompatActivity() {
         ABoom.init(this)
         Platform.platform = Platform.ANDROID
         androidRenderer = ABoom.boom.androidRendered
-        setContentView(androidRenderer.renderComponent(
-                FlatList(),
-                FlatListProps(
-                        length = 30,
-                        onRenderRow = { viewType ->
-                            h(::Counter)
-                        },
-                        onUpdateRow = { view, position -> }
-                )
-        ))
+        setContentView(androidRenderer.renderComponent(MainComponent(), null))
     }
 }
---
+++ app/src/main/java/nice/com/a7boom/MainComponent.kt
@@ -0,0 +1,21 @@
+package nice.com.a7boom
+
+import vm.component.Component
+import vm.counter.FlatList
+import vm.counter.FlatListProps
+import vm.vnode.VNode
+import vm.vnode.h
+import vm.counter.Counter
+
+class MainComponent : Component<Any?, Any?>(null) {
+    override var props: Any? = null
+
+    override fun render(): VNode<*> = h(
+            ::FlatList, FlatListProps(
+            length = 30,
+            onRenderRow = { viewType ->
+                h(::Counter)
+            },
+            onUpdateRow = { view, position -> }
+    ))
+}
